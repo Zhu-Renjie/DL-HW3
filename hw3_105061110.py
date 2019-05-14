7343c465c9a36ab3be9716092dee956eefcbda06
Thomas Chou <thomaschou861205@gmail.com>
2019-05-14 23:00:07 +0800

    frame work

---
+++ hw3_105061110.py
@@ -0,0 +1,24 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+class CNN():
+    def __init__(self):
+        pass
+    def add_layer(self):
+        pass
+    def train(self):
+        pass
+    def evaluate(self):
+        pass
+    def predict(self):
+        pass
+    def get_metrics(self):
+        pass
+
+def preprossed():
+    pass
+
+if __name__ == "__main__":
+    pass
