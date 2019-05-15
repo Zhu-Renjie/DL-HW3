43eefc73bd7ca317ecf34efa9b886d3098b7d9cb
Thomas Chou <thomaschou861205@gmail.com>
2019-05-16 01:13:32 +0800

    add onehot_encoded

--- data_prepro.py
+++ data_prepro.py
@@ -4,7 +4,8 @@ from os.path import isfile, isdir, join
 from scipy import misc
 import matplotlib.pyplot as plt
 import numpy as np
-
+from numpy import array, argmax
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 def data_prepro(folder=None, root=None):
     if root is not None:
@@ -16,7 +17,9 @@ def data_prepro(folder=None, root=None):
     files = listdir(path)
     syllable = path.split('\\')
 
+    i = -1
     for f in files:
+        i += 1
         fullpath = join(path, f)
         if isfile(fullpath):
             # print("file：", f)
@@ -28,19 +31,22 @@ def data_prepro(folder=None, root=None):
                 face = rgb2gray(face)
 
             if syllable[0] == 'test':
-                newpath  = "prepro_test" + fullpath[4:]
+                # e.g. prepro_test\accordion_0.jpg
+                newpath = "prepro_test" + "\\{}_{}.jpg".format(syllable[1], i)
+                print(newpath)
             elif syllable[0] == 'train':
-                newpath  = "prepro_train" + fullpath[5:]
+                newpath = "prepro_train" + "\\{}_{}.jpg".format(syllable[1], i)
+                # print(newpath)
             misc.imsave(newpath, face)
             # plt.imshow(face,cmap=plt.cm.gray)
             # plt.show()
 
         elif isdir(fullpath):
             # print("folder：", f)
-            if syllable[0] == 'test':
-                createFolder("prepro_test\\"+f)
-            elif syllable[0] == 'train':
-                createFolder("prepro_train\\"+f)
+            # if syllable[0] == 'test':
+            #     createFolder("prepro_test\\"+f)
+            # elif syllable[0] == 'train':
+            #     createFolder("prepro_train\\"+f)
             data_prepro(folder=f, root=path)
 
 def rgb2gray(rgb):
@@ -53,8 +59,44 @@ def createFolder(directory):
     except OSError:
         print ('Error: Creating directory. ' +  directory)
 
+def label_generator(path=None):
+    files = listdir(path)
+    X = []
+    y = []
+    for f in files:
+        fullpath = join(path, f)
+        if isfile(fullpath):
+            label = '_'.join((f.split('_'))[:-1])
+            face = misc.imread(fullpath,'L')
+            X.append(face)
+            y.append(label)
+            # print("file：", label)
+            # plt.imshow(face,cmap=plt.cm.gray)
+            # plt.show()
+
+    values = array(y)
+    
+    # integer encode
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(values)
+    # print(integer_encoded)
+
+    # binary encode
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    # print(onehot_encoded)
+    
+    # invert first example
+    # inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
+    # print(inverted)
+    
+    return X, y, onehot_encoded
+
+
 if __name__ == "__main__":
     ## prepose training set and testing set
     # data_prepro(root="test")
     # data_prepro(root="train")
-    pass
+    X, y, onehot_encoded = label_generator("prepro_test")
+    # X, y, classes = label_generator("prepro_train")
